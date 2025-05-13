14617818fbee39f2443895f2729e8153b88f864f
dhidrod <adanielhidalgo@gmail.com>
2025-05-13 16:09:22 +0200

    Add Like model and migration with user_id and image_id relationships

--- database/migrations/2025_05_13_133226_create_likes_table.php
+++ database/migrations/2025_05_13_133226_create_likes_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('image_id')->constrained('images')->onDelete('cascade');
             $table->timestamps();
+            $table->unique(['user_id', 'image_id']);
+
         });
     }
 
