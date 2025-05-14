99bc82d3795f0c16b8c88d2478dcf1950a0a2ac1
dhidrod <adanielhidalgo@gmail.com>
2025-05-14 19:10:32 +0200

    Refactor header to conditionally change background color for admin users and display welcome message

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,27 @@
-<header class="bg-[#161615] shadow-md p-4">
+<header
+    class="{{ auth()->check() && auth()->user()->hasRole('admin') ? 'bg-[#b88b38]' : 'bg-[#161615]' }} shadow-md p-4">
     <nav class="container mx-auto px-4 py-2 flex justify-between items-center">
         <a href="/" class="text-4xl font-bold text-[#EDEDEC]">
             Red Social
         </a>
 
         <ul class="flex gap-6 text-[#EDEDEC]">
-            <li>
-                <a href="/" class="nav-link">Inicio</a>
-            </li>
-            <li>
-                <a href="/dashboard" class="nav-link">Dashboard</a>
-            </li>
-            <li>
-                <a href="/register" class="nav-link">Nuevo Usuario</a>
-            </li>
-            <li>
-                <a href="/admin" class="nav-link">Admin</a>
-            </li>
+                    @role('admin')
+                    <p>Bienvenido, Administrador.</p>
+                    @endrole
+
+                    <li>
+                        <a href="/" class="nav-link">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard" class="nav-link">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="/register" class="nav-link">Nuevo Usuario</a>
+                    </li>
+                    <li>
+                        <a href="/admin" class="nav-link">Admin</a>
+                    </li>
         </ul>
     </nav>
 </header>
